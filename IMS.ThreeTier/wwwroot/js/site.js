e6326830fab043fe4f9332c4392cff7b91f7b135
Rifat Bin Aziz <rifatbinaziz.1998@gmail.com>
2026-05-14 22:57:12 +0600

    refactor: introduce reusable partial modal component

---
+++ IMS.ThreeTier/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿function showModal(title, message, type = "success") {
+
+    $("#modalTitle").text(title);
+
+    $("#modalMessage").text(message);
+
+    let header = $("#msgModal .modal-header");
+
+    header.removeClass(
+        "bg-success bg-danger bg-warning text-white"
+    );
+
+    if (type === "success") {
+        header.addClass("bg-success text-white");
+    }
+    else if (type === "error") {
+        header.addClass("bg-danger text-white");
+    }
+
+    var modal = new bootstrap.Modal(
+        document.getElementById("msgModal")
+    );
+
+    modal.show();
+}
