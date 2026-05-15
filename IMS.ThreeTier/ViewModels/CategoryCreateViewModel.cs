1fe57e88c8b6af37c7693399b39a7c504c8029b1
Rifat Bin Aziz <rifatbinaziz.1998@gmail.com>
2026-05-16 00:27:22 +0600

    refactor(category): add required check on category name in viewmodel and UI

--- IMS.ThreeTier/ViewModels/CategoryCreateViewModel.cs
+++ IMS.ThreeTier/ViewModels/CategoryCreateViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace IMS.WEB.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WEB.ViewModels
 {
     public class CategoryCreateViewModel
     {
+        [Required(ErrorMessage = "Category name is required.")]
         public string Name { get; set; }
         public string? Description { get; set; }
     }
