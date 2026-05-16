86eb7e0c6831244e2d7540299fc45b9412bca21c
Rifat Bin Aziz <rifatbinaziz.1998@gmail.com>
2026-05-16 20:18:11 +0600

    refactor(category): upgrade category ui to match product ui.

--- IMS.ThreeTier/ViewModels/CategoryListViewModel.cs
+++ IMS.ThreeTier/ViewModels/CategoryListViewModel.cs
@@ -1,14 +1,16 @@
-﻿using IMS.WEB.ViewModels;
-
-public class CategoryListViewModel
+﻿namespace IMS.WEB.ViewModels
 {
-    public List<CategoryViewModel> Categories { get; set; }
 
-    public int Page { get; set; }
+    public class CategoryListViewModel
+    {
+        public List<CategoryViewModel> Categories { get; set; }
 
-    public int PageSize { get; set; }
+        public int Page { get; set; }
 
-    public int TotalPages { get; set; }
+        public int PageSize { get; set; }
 
-    public string? Search { get; set; }
+        public int TotalPages { get; set; }
+
+        public string? Search { get; set; }
+    }
 }
