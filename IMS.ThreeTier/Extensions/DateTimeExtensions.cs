7bb7495b4864168196cc5689588b665747e5b619
Rifat Bin Aziz <rifatbinaziz.1998@gmail.com>
2026-05-25 23:06:05 +0600

    feature: add datetime as dd/MM/yyyy HH:mm throuout all existing ui

---
+++ IMS.ThreeTier/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace IMS.WEB.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToUiDate(
+            this DateTime date)
+        {
+            return date.ToString(
+                "dd/MM/yyyy HH:mm");
+        }
+
+        public static string ToUiDate(
+            this DateTime? date)
+        {
+            if (!date.HasValue)
+                return "-";
+
+            return date.Value.ToString(
+                "dd/MM/yyyy HH:mm");
+        }
+    }
+}
